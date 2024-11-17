83dca7711b476b52a12cc6d839d53304fed7f69a
Vordgi <sasha2822222@gmail.com>
2024-11-17 19:57:30 +0400

    cs-14 update mock data schemas

---
+++ src/core/mock/speakers.ts
@@ -0,0 +1,26 @@
+export const speakers = [
+    {
+        slug: "vadim-makeev",
+        name: "Vadim Makeev",
+    },
+    {
+        slug: "maxim-khalizov",
+        name: "Maxim Khalizov",
+    },
+    {
+        slug: "tanya-fokina",
+        name: "Tanya Fokina",
+    },
+    {
+        slug: "andrey-evdokov",
+        name: "Andrey Evdokov",
+    },
+    {
+        slug: "ilona-sarkisova",
+        name: "Ilona Sarkisova",
+    },
+    {
+        slug: "philip-baranovsky",
+        name: "Philip Baranovsky",
+    },
+];
