69a6147f2385994074ac91e50bbf532e5653d270
Vordgi <sasha2822222@gmail.com>
2025-01-16 19:49:57 +0400

    cs-27 button component

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,31 +1,23 @@
 import cn from "classnames";
 
+import "./button.scss";
+
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
     variant?: keyof typeof BUTTON_VARIANTS;
     size?: keyof typeof BUTTON_SIZES;
 }
 
 const BUTTON_VARIANTS = {
-    primary: "bg-blue-700 text-slate-50 hover:bg-blue-600",
-    secondary: "bg-orange-700 text-slate-50 hover:bg-orange-600",
-    neutral: "bg-slate-800 text-slate-50 hover:bg-slate-600",
+    primary: "button_primary",
+    secondary: "button_secondary",
+    neutral: "button_neutral",
 };
 
 const BUTTON_SIZES = {
-    sm: "py-1 px-4",
-    md: "py-2 px-6",
-    lg: "py-3 px-8",
-    xl: "py-4 px-10",
+    md: "button_medium",
+    lg: "button_large",
 };
 
-export const Button: React.FC<ButtonProps> = ({ className, variant, size = "md", ...props }) => (
-    <button
-        className={cn(
-            "flex items-center rounded-md",
-            variant && BUTTON_VARIANTS[variant],
-            size && BUTTON_SIZES[size],
-            className,
-        )}
-        {...props}
-    />
+export const Button: React.FC<ButtonProps> = ({ className, variant = "neutral", size = "md", ...props }) => (
+    <button className={cn("button", BUTTON_VARIANTS[variant], BUTTON_SIZES[size], className)} {...props} />
 );
