ebd67c9eb2cd760592173284d349cc781a1f8f96
Vordgi <sasha2822222@gmail.com>
2025-01-23 20:24:07 +0400

    cs-45 call for speakers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomeIntro } from "../components/sections/home-intro";
 import { AboutCommunity } from "../components/sections/about-community";
+import { CallForSpeakers } from "../components/sections/call-for-speakers";
 
 const HomePage = () => (
     <>
         <HomeIntro />
         <AboutCommunity />
+        <CallForSpeakers />
     </>
 );
 
---
+++ src/components/sections/call-for-speakers/index.tsx
@@ -0,0 +1,22 @@
+import { Button } from "../../ui/button";
+import { Block } from "../../ui/block";
+
+import "./call-for-speakers.scss";
+
+export const CallForSpeakers = () => (
+    <Block id="intro" bodyClassName="call-for-speakers-body">
+        <div className="call-for-speakers-head">
+            <h2 className="call-for-speakers-heading">Call for speakers</h2>
+            <p className="call-for-speakers-subheading">Passionate about tech and design</p>
+        </div>
+        <div className="call-for-speakers-info">
+            <p className="call-for-speakers-description">
+                If you want to share your experience and knowledge in upcoming Tbilisi JS Meetup, let us know till
+                01.16.25
+            </p>
+            <Button variant="primary" className="call-for-speakers-button" size="md">
+                Apply to speak
+            </Button>
+        </div>
+    </Block>
+);
