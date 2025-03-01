ff763a1632f7f40293d5a23db2903e87e29608ff
Vordgi <sasha2822222@gmail.com>
2025-03-01 15:25:22 +0400

    cs-92 metadata for events page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,10 +1,12 @@
+import { type Metadata } from "next/types";
+
 import { StarsWrapper } from "@src/components/elements/stars-wrapper";
 import { EventsIntro } from "@src/components/sections/events-intro";
 import { EventsList } from "@src/components/sections/events-list";
-import { CallForSpeakers } from "../../components/sections/call-for-speakers";
-import { JoinUs } from "../../components/sections/join-us";
-import { CatsPattern } from "../../components/sections/cats-pattern";
-import { CommunityGallery } from "../../components/sections/community-gallery";
+import { CallForSpeakers } from "@src/components/sections/call-for-speakers";
+import { JoinUs } from "@src/components/sections/join-us";
+import { CatsPattern } from "@src/components/sections/cats-pattern";
+import { CommunityGallery } from "@src/components/sections/community-gallery";
 
 const EventsPage = () => (
     <>
@@ -28,5 +30,16 @@ const EventsPage = () => (
         </StarsWrapper>
     </>
 );
+
+export const metadata: Metadata = {
+    title: "Tbilisi JS Events",
+    description:
+        "Bringing together experts and enthusiasts to change the future of the web - conferences about technologies, trends and people",
+    openGraph: {
+        type: "website",
+        images: "/preview.jpg",
+        locale: "en_US",
+    },
+};
 
 export default EventsPage;
