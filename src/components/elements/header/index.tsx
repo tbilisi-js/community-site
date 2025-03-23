6cef29f62a4e5e428415c948a45f0b7bafc054ee
Vordgi <sasha2822222@gmail.com>
2025-03-23 12:06:48 +0400

    cs-55 navigation and mobile menu

--- src/components/elements/header/index.tsx
+++ src/components/elements/header/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import cn from "classnames";
 
 import { Button } from "@src/components/ui/button";
@@ -8,29 +9,25 @@ import { NavLink } from "@src/components/ui/nav-link";
 import "./header.scss";
 
 export const Header = () => {
+    const [opened, setOpened] = useState(false);
+
     return (
-        <header className="header">
+        <header className={cn("header", opened && "_opened")}>
             <div className="header-content">
                 <NavLink href="/" className="header-logo">
                     <img src="/logo.png" alt="TJS" width={52} height={30} />
                 </NavLink>
                 <nav className="header-nav">
-                    <div className={cn("header-nav-list")}>
-                        {/* <NavLink href="/" className="header-nav-link">
-                            Home
-                        </NavLink>
+                    <div className="header-nav-list">
                         <NavLink href="/events" className="header-nav-link">
                             Events
                         </NavLink>
-                        <NavLink href="/speakers" className="header-nav-link">
-                            Speakers
+                        <NavLink href="/people" className="header-nav-link">
+                            People
                         </NavLink>
-                        <NavLink href="/partners" className="header-nav-link">
+                        <NavLink href="/become-a-partner" className="header-nav-link">
                             Partners
                         </NavLink>
-                        <NavLink href="/contacts" className="header-nav-link">
-                            Contacts
-                        </NavLink> */}
                         <Button
                             variant="primary"
                             size="md"
@@ -42,6 +39,9 @@ export const Header = () => {
                             Join Us
                         </Button>
                     </div>
+                    <Button variant="primary" size="md" className="header-menu" onClick={() => setOpened(!opened)}>
+                        Menu
+                    </Button>
                 </nav>
             </div>
         </header>
