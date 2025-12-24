000752a8e5d871f919aefa5633cf937c8e46ce4f
Vordgi <sasha2822222@gmail.com>
2025-12-24 10:35:11 +0000

    cs-133 update meta information

--- src/core/mock/organisers.ts
+++ src/core/mock/organisers.ts
@@ -1,18 +1,11 @@
 import juliaVolkova from "../mock/images/organisers/julia_volkova.png";
-import juliaVolkovaMask from "../mock/images/organisers/mask_volkova.png";
 import antonPavlov from "../mock/images/organisers/anton_pavlov.jpg";
-import antonPavlovMask from "../mock/images/organisers/mask_pavlov.png";
-import alexSavelyev from "../mock/images/organisers/alex_savelyev.png";
-import alexSavelyevMask from "../mock/images/organisers/mask_savelyev.png";
 import anastasiaPalamarchuk from "../mock/images/organisers/anastasia_palamarchuk.png";
-import anastasiaPalamarchukMask from "../mock/images/organisers/mask_palamarchuk.png";
 import aleksandrKorotaev from "../mock/images/organisers/aleksandr_korotaev.png";
-import aleksandrKorotaevMask from "../mock/images/organisers/mask_korotaev.png";
 
 export const organisers = [
     {
         avatarSrc: juliaVolkova.src,
-        maskImage: juliaVolkovaMask.src,
         name: "Julia Volkova",
         id: "volkova",
         description: "Frontend Expert at Hyperskill and Mentor at HTML Academy",
@@ -20,23 +13,13 @@ export const organisers = [
     },
     {
         avatarSrc: antonPavlov.src,
-        maskImage: antonPavlovMask.src,
         name: "Anton Pavlov",
         id: "pavlov",
         description: "DevOps",
         socialLink: "https://www.linkedin.com/in/antonepavlov/",
     },
     {
-        avatarSrc: alexSavelyev.src,
-        maskImage: alexSavelyevMask.src,
-        name: "Alex Savelyev",
-        id: "savelyev",
-        description: "Frontend Developer",
-        socialLink: "https://www.linkedin.com/in/vordgi/",
-    },
-    {
         avatarSrc: anastasiaPalamarchuk.src,
-        maskImage: anastasiaPalamarchukMask.src,
         name: "Anastasia Palamarchuk",
         id: "palamarchuk",
         description: "Tech Recruiter",
@@ -44,7 +27,6 @@ export const organisers = [
     },
     {
         avatarSrc: aleksandrKorotaev.src,
-        maskImage: aleksandrKorotaevMask.src,
         name: "Aleksandr Korotaev",
         id: "korotaev",
         description: "Frontend Developer, SPB Frontend Organizer",
