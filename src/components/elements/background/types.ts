47cdc1ccabe23c5af2bf04b428af6bd2e5526068
Vordgi <sasha2822222@gmail.com>
2025-12-27 16:26:49 +0000

    cs-137 add bg addons for xmas

--- src/components/elements/background/types.ts
+++ src/components/elements/background/types.ts
@@ -10,7 +10,7 @@ export type ElementViewType =
     | "snow3"
     | "orange";
 export type ElementViewSize = 6 | 10 | 16 | 18 | 22 | 36 | 40 | 52 | 64 | 80 | 92;
-export type ElementViewAddon = "pulse" | "rotate-15-left" | "rotate-15-right" | "fly";
+export type ElementViewAddon = "pulse" | "rotate-15-left" | "rotate-15-right" | "fly" | "fall";
 export type ElementView = { type: ElementViewType; size: ElementViewSize; addons?: ElementViewAddon[] };
 
 export type Element = {
