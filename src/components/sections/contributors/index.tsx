bd8bc6c487c5aee1957a2ae13add9bc8e5572215
Vordgi <sasha2822222@gmail.com>
2025-12-27 22:06:34 +0000

    cs-139 move svg filters

--- src/components/sections/contributors/index.tsx
+++ src/components/sections/contributors/index.tsx
@@ -3,59 +3,14 @@ import Link from "next/link";
 
 import { speakers } from "@src/core/mock/speakers";
 import { Block } from "@src/components/ui/block";
+import { SvgFilters } from "@src/components/ui/svg-filters";
 
 import "./contributors.scss";
 
 export const Contributors = () => {
     return (
         <Block id="contributors" bodyClassName="contributors-body">
-            <svg className="contributors-svg-filters">
-                <defs>
-                    <filter id="displacementFilter" x="0" y="0" width="100px" height="100px">
-                        <feTurbulence type="turbulence" baseFrequency="2" numOctaves="20" result="turbulence">
-                            <animate attributeName="baseFrequency" values="2;6;2" dur="50s" repeatCount="indefinite" />
-                        </feTurbulence>
-                        <feDisplacementMap
-                            in2="turbulence"
-                            in="SourceGraphic"
-                            scale="-0.2"
-                            xChannelSelector="R"
-                            yChannelSelector="G"
-                        />
-                        <feDisplacementMap
-                            in2="turbulence"
-                            in="SourceGraphic"
-                            scale="0.2"
-                            xChannelSelector="R"
-                            yChannelSelector="G"
-                        />
-                    </filter>
-                    <mask
-                        id="displacementMaskDynamic"
-                        maskUnits="objectBoundingBox"
-                        maskContentUnits="objectBoundingBox"
-                    >
-                        <circle cx="0.45" cy="0.45" r="0.45" fill="white" filter="url(#displacementFilter)" />
-                    </mask>
-                    <filter id="displacementFilter2" x="0" y="0" width="100px" height="100px">
-                        <feTurbulence type="turbulence" baseFrequency="20" numOctaves="1" result="turbulence2" />
-                        <feDisplacementMap
-                            in2="turbulence2"
-                            in="SourceGraphic"
-                            scale="0.2"
-                            xChannelSelector="R"
-                            yChannelSelector="G"
-                        />
-                    </filter>
-                    <mask
-                        id="displacementMaskStatic"
-                        maskUnits="objectBoundingBox"
-                        maskContentUnits="objectBoundingBox"
-                    >
-                        <circle cx="0.45" cy="0.45" r="0.45" fill="white" filter="url(#displacementFilter2)" />
-                    </mask>
-                </defs>
-            </svg>
+            <SvgFilters filters={["displacement-filter-dynamic", "displacement-filter-static"]} />
             <h2 className="contributors-heading">Meet the voices</h2>
             <ul className="contributors-list">
                 {speakers.map((speaker) => (
